83a87f14b2db1ac92836b7f32d81d5e9ed015a37
Joey Chan <joseph.chan@canada.ca>
2017-09-26 09:56:25 -0400

    Add required lanaguages

--- parser/md-element.parser.ts
+++ parser/md-element.parser.ts
@@ -25,9 +25,12 @@ export class MDElementParser {
             [].concat(raw['element']).forEach(e => elements.push(MDElementParser.parse(e)));
         }
 
-        const requiredLanguages = [];
+        let english: boolean;
+        let french: boolean;
         if (raw['required-language'] !== undefined) {
-            [].concat(raw['required-language']).forEach(rl => requiredLanguages.push({name: rl['@name']}))
+            const requiredLanguages = [].concat(raw['required-language']).map(lang => lang['@name']);
+            english = requiredLanguages.indexOf('en') !== -1;
+            french = requiredLanguages.indexOf('fr') !== -1;
         }
 
         const element: MDElement = {
@@ -35,7 +38,7 @@ export class MDElementParser {
             group: raw['@group'],
             id: raw['@id'],
             index: raw['@index'],
-            languageSensitive: raw['@language-sensitive'],
+            languageSensitive: raw['@language-sensitive'] === 'true' ? true : false,
             max: String(raw['@max']),
             min: String(raw['@min']),
             name: raw['@name'],
@@ -46,7 +49,10 @@ export class MDElementParser {
             pattern: raw['@pattern'],
             enums: enums,
             elements: elements,
-            requiredLanguages: requiredLanguages
+            requiredLanguages: {
+                english: english,
+                french: french
+            }
         };
 
         return element;
