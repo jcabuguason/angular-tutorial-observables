c5ed26212542f02f4e12e22dc229c857df97da3b
Choi <Paul.Choi@ec.gc.ca>
2017-10-02 13:16:02 -0400

    add words and alphabetize langconstants

--- object/LanguageConstants.ts
+++ object/LanguageConstants.ts
@@ -1,154 +1,162 @@
 export const LanguageConstants = {
-  'main': {
-    'en': 'Metadata Instance',
-    'fr': 'Instance de Métadonnées'
+  'about': {
+      'en': 'About',
+      'fr': 'Sur'
+  },
+  'about_pegasus': {
+      'en': 'About Pegasus',
+      'fr': 'À propos de Pegasus'
+  },
+  'about_this': {
+      'en': 'About this site',
+      'fr': 'À propos de ce message'
+  },
+  'about_web': {
+      'en': 'About the Web Standards',
+      'fr': 'À propos des normes Web'
   },
   'add': {
-    'en': 'Add',
-    'fr': 'Ajouter'
-  },
-  'view': {
-    'en': 'View',
-    'fr': 'Vue'
-  },
-  'update': {
-    'en': 'Update',
-    'fr': 'Mettre à jour'
-  },
-  'definition': {
-    'en': 'Definition',
-    'fr': 'Définition'
-  },
-  'metadata': {
-    'en': 'Metadata',
-    'fr': 'Métadonnées'
-  },
-  'welcome': {
-    'en': 'Pegasus is capable of managing metadata instances with the following features:',
-    'fr': 'Pegasus est capable de gérer les instances de métadonnées avec les fonctionnalités suivantes:'
-  },
-  'noneFound': {
-    'en': 'No instances found for this definition. Please choose a different one.',
-    'fr': 'Aucune occurrence n\'a été trouvée pour cette définition. Choisissez un autre.'
-  },
-  'definition_version': {
-    'en': 'Definition Version',
-    'fr': 'Version de Définition'
-  },
-  'instance_version': {
-    'en': 'Instance Version',
-    'fr': 'Version de L\'instance'
+      'en': 'Add',
+      'fr': 'Ajouter'
   },
   'add_metadata': {
-    'en': 'Add Metadata Instance',
-    'fr': 'Ajouter une instance de métadonnées'
-  },
-  'reason_for': {
-    'en': 'Reason for change',
-    'fr': 'Raison du changement'
-  },
-  'effective_start': {
-    'en': 'Effective start',
-    'fr': 'Démarrage effectif'
-  },
-  'effective_end': {
-    'en': 'Effective end',
-    'fr': 'Fin effective'
-  },
-  'taxonomy': {
-    'en': 'Taxonomy',
-    'fr': 'Taxonomie'
+      'en': 'Add Metadata Instance',
+      'fr': 'Ajouter une instance de métadonnées'
   },
   'collapse_all': {
-    'en': 'Collapse All',
-    'fr': 'Réduire tout'
+      'en': 'Collapse All',
+      'fr': 'Réduire tout'
   },
-  'update_metadata': {
-    'en': 'Update Metadata Instance',
-    'fr': 'Mise à jour de l\'instance de métadonnées'
+  'confirm': {
+      'en': 'Confirm',
+      'fr': 'Confirmer'
+  },
+  'contact': {
+      'en': 'Contact Us',
+      'fr': 'Contactez nous'
+  },
+  'date_modified': {
+      'en': 'Date Modified',
+      'fr': 'Date Modifiée'
+  },
+  'definition': {
+      'en': 'Definition',
+      'fr': 'Définition'
+  },
+  'definition_version': {
+      'en': 'Definition Version',
+      'fr': 'Version de Définition'
+  },
+  'domain': {
+      'en': 'Domain',
+      'fr': 'Domaine'
+  },
+  'effective_end': {
+      'en': 'Effective end',
+      'fr': 'Fin effective'
+  },
+  'effective_start': {
+      'en': 'Effective start',
+      'fr': 'Démarrage effectif'
+  },
+  'errors': {
+    'en': 'Errors',
+    'fr': 'Les erreurs'
+  },
+  'input_required': {
+    'en': 'Please input a value',
+    'fr': 'Inscrivez une valeur'
   },
   'instance_id': {
     'en': 'Instance ID',
     'fr': 'ID de l\'instance'
   },
-  'submit': {
-    'en': 'Submit',
-    'fr': 'Soumettre'
-  },
-  'view_metadata': {
-    'en': 'View Metadata Instance',
-    'fr': 'Afficher l\'instance de métadonnées'
-  },
-  'query_param': {
-    'en': 'Please insert a query parameter for an instance of ',
-    'fr': 'Veuillez insérer un paramètre de requête pour une instance de '
+  'instance_version': {
+    'en': 'Instance Version',
+    'fr': 'Version de L\'instance'
   },
   'language': {
-    'en': 'English',
-    'fr': 'Français'
-  },
-  'contact': {
-    'en': 'Contact Us',
-    'fr': 'Contactez nous'
-  },
-  'about': {
-    'en': 'About',
-    'fr': 'Sur'
-  },
-  'about_this': {
-    'en': 'About this site',
-    'fr': 'À propos de ce message'
-  },
-  'questions': {
-    'en': 'Questions or comments?',
-    'fr': 'Questions ou commentaires?'
-  },
-  'about_pegasus': {
-    'en': 'About Pegasus',
-    'fr': 'À propos de Pegasus'
-  },
-  'about_web': {
-    'en': 'About the Web Standards',
-    'fr': 'À propos des normes Web'
-  },
-  'stay_connected': {
-    'en': 'Stay connected',
-    'fr': 'Rester connecté'
-  },
-  'stability': {
-    'en': 'DMS Stability',
-    'fr': 'Stabilité DMS'
-  },
-  'transparency': {
-    'en': 'Transparency',
-    'fr': 'Transparence'
-  },
-  'date_modified': {
-    'en': 'Date Modified',
-    'fr': 'Date Modifiée'
+      'en': 'English',
+      'fr': 'Français'
   },
   'login': {
-    'en': 'Login',
-    'fr': 'S\'identifier'
+      'en': 'Login',
+      'fr': 'S\'identifier'
   },
   'logout': {
-    'en': 'Logout',
-    'fr': 'Connectez - Out'
+      'en': 'Logout',
+      'fr': 'Connectez - Out'
   },
-  'domain': {
-    'en': 'Domain',
-    'fr': 'Domaine'
+  'main': {
+      'en': 'Metadata Instance',
+      'fr': 'Instance de Métadonnées'
   },
-  'username': {
-    'en': 'Username',
-    'fr': 'Nom d\'utilisateur'
+  'metadata': {
+      'en': 'Metadata',
+      'fr': 'Métadonnées'
+  },
+  'noneFound': {
+      'en': 'No instances found for this definition. Please choose a different one.',
+      'fr': 'Aucune occurrence n\'a été trouvée pour cette définition. Choisissez un autre.'
   },
   'password': {
-    'en': 'Password',
-    'fr': 'Mot de Passe'
+      'en': 'Password',
+      'fr': 'Mot de Passe'
   },
-  'confirm': {
-    'en': 'Confirm',
-    'fr': 'Confirmer'
+  'query_param': {
+      'en': 'Please insert a query parameter for an instance of ',
+      'fr': 'Veuillez insérer un paramètre de requête pour une instance de '
+  },
+  'questions': {
+      'en': 'Questions or comments?',
+      'fr': 'Questions ou commentaires?'
+  },
+  'reason_for': {
+      'en': 'Reason for change',
+      'fr': 'Raison du changement'
+  },
+  'stability': {
+      'en': 'DMS Stability',
+      'fr': 'Stabilité DMS'
+  },
+  'stay_connected': {
+      'en': 'Stay connected',
+      'fr': 'Rester connecté'
+  },
+  'submit': {
+      'en': 'Submit',
+      'fr': 'Soumettre'
+  },
+  'taxonomy': {
+      'en': 'Taxonomy',
+      'fr': 'Taxonomie'
+  },
+  'transparency': {
+      'en': 'Transparency',
+      'fr': 'Transparence'
+  },
+  'update': {
+      'en': 'Update',
+      'fr': 'Mettre à jour'
+  },
+  'update_metadata': {
+      'en': 'Update Metadata Instance',
+      'fr': 'Mise à jour de l\'instance de métadonnées'
+  },
+  'username': {
+      'en': 'Username',
+      'fr': 'Nom d\'utilisateur'
+  },
+  'view': {
+      'en': 'View',
+      'fr': 'Vue'
+  },
+  'view_metadata': {
+      'en': 'View Metadata Instance',
+      'fr': 'Afficher l\'instance de métadonnées'
+  },
+  'welcome': {
+      'en': 'Pegasus is capable of managing metadata instances with the following features:',
+      'fr': 'Pegasus est capable de gérer les instances de métadonnées avec les fonctionnalités suivantes:'
   }
 }
