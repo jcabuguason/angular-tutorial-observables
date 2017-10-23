6c7feac833b1f81c0c5ae443eaa6f65436d96a8c
Choi <Paul.Choi@ec.gc.ca>
2017-10-23 14:59:00 -0400

    add necessary packages for charts

--- commons.module.ts
+++ commons.module.ts
@@ -2,12 +2,14 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgxChartsModule
   ]
 })
 export class CommonsModule {
