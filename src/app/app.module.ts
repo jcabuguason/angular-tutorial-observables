46fc3a260c6fda435d3e7519f69940eceed483c9
Ben Shaft <ben.shaft@canada.ca>
2017-12-01 13:59:07 -0500

    CLI structure with PlaceHolder Module

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+import { PlaceHolderModule } from './modules/place-holder/place-holder.module';
+import { PlaceHolderComponent } from './modules/place-holder/place-holder.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PlaceHolderComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
