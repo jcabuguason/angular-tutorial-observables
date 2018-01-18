91bfddca611a2c3f80a3e642e3c747381633ceca
Joey Chan <joseph.chan@canada.ca>
2018-01-18 15:32:44 -0500

    issue 16643: Update ES Service

--- src/lib/elastic-search/src/model/observations-from-elements-params.model.ts
+++ src/lib/elastic-search/src/model/observations-from-elements-params.model.ts
@@ -1,5 +1,5 @@
 import { CommonElasticSearchParams } from './common-elastic-search-params.model';
 
 export interface ObservationsFromElementsParams extends CommonElasticSearchParams {
-
+  operator?: 'AND' | 'OR';
 }
