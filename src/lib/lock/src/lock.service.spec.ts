057ef2efe89bb8e84f6805dd9a47fd5bfa6cb5c8
Ben Shaft <ben.shaft@canada.ca>
2018-03-20 15:50:42 -0400

    Updated old tests, added 'always-fail' tests to some classes

--- src/lib/lock/src/lock.service.spec.ts
+++ src/lib/lock/src/lock.service.spec.ts
@@ -46,11 +46,11 @@ describe('LockService', () => {
       timeout: 'yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\''
     };
 
-    service.aquireLock({resource_id: ['1', '2'], type: 'number', reset_enabled: true}).subscribe((response) => {
+    service.acquireLock({resource_id: ['1', '2'], type: 'number', reset_enabled: true}).subscribe((response) => {
       expect(response).toBe(result);
     });
 
-    const req = httpMock.expectOne(`${config.endpoint}/lock/aquire_lock`);
+    const req = httpMock.expectOne(`${config.endpoint}?url=/lock/acquire_lock`);
     expect(req.request.body).toEqual({resource_id: ['1', '2'], type: 'number', reset_enabled: true});
     req.flush(result);
   });
@@ -68,19 +68,28 @@ describe('LockService', () => {
       expect(response).toEqual(result);
     });
 
-    const req = httpMock.expectOne(`${config.endpoint}/lock/release_lock`);
+    const req = httpMock.expectOne(`${config.endpoint}?url=/lock/release_lock`);
     expect(req.request.body).toEqual({resource_id: ['1'], type: 'number'});
     req.flush(result);
   });
 
   it('should make a call to get lock info', () => {
 
+    const result = {
+      resource_id: '1',
+      type: 'number',
+      user: '',
+      user_first_name: '',
+      user_last_name: '',
+      timeout: '',
+    };
+
     service.lockInfo({resource_id: '1', type: 'number'}).subscribe((response) => {
-      expect(response).toEqual('');
+      expect(response).toEqual(result);
     });
-    const req = httpMock.expectOne(request => request.method === 'GET' && request.url === `${config.endpoint}/lock/lock_info`);
+    const req = httpMock.expectOne(request => request.method === 'GET' && request.url === `${config.endpoint}?url=/lock/lock_info`);
     expect(req.request.params.get('resource_id')).toEqual('1');
     expect(req.request.params.get('type')).toEqual('number');
-    req.flush('');
+    req.flush(result);
   });
 });
--- src/lib/metadata/src/parser/md-element.parser.spec.ts
+++ src/lib/metadata/src/parser/md-element.parser.spec.ts
@@ -2,7 +2,8 @@ import { MDElementParser } from './md-element.parser';
 import { ELEMENT_PARSER_INPUT, ELEMENT_PARSER_OUTPUT } from './parser-testing-constants';
 
 describe('MDElementParser', () => {
-
+  // Broken Test
+  // element parse adds in optional `requiredLanguages: {english: undefined, french: undefined}` field
   it('should return the correct value', () => {
     expect(MDElementParser.parse(ELEMENT_PARSER_INPUT)).toEqual(ELEMENT_PARSER_OUTPUT);
   });
