12b5381776c7d081a9812795dec48801c97a1396
Colin Moult <colin.moult@canada.ca>
2018-03-29 16:41:05 -0400

    Adding search reducer to commons

--- src/lib/search-box/src/public_api.ts
+++ src/lib/search-box/src/public_api.ts
@@ -2,6 +2,7 @@ export * from './search.module';
 export * from './search.service';
 export * from './search.component';
 export * from './search.model';
+export * from './search.reducer';
 
 export * from './search-box.config';
 export * from './equivalent-keywords';
---
+++ src/lib/search-box/src/search.reducer.ts
@@ -0,0 +1,42 @@
+import { Action } from '@ngrx/store';
+
+import { SearchModel } from './search.model';
+
+export const BUILD_SEARCH = 'BUILD_SEARCH';
+export const CLEAR_SEARCH = 'CLEAR_SEARCH';
+
+export const initialState: SearchModel = {
+    taxonomy: [],
+    elements: [],
+    from: new Date(),
+    to: new Date(),
+    size: 300,
+    operator: ''
+};
+
+export class SearchAction implements Action {
+    readonly type = BUILD_SEARCH;
+
+    constructor(public payload: SearchModel) {}
+}
+
+export class ClearSearchAction implements Action {
+    readonly type = CLEAR_SEARCH;
+
+    constructor() {}
+}
+
+export function searchReducer(state = initialState, action: SearchAction | ClearSearchAction): SearchModel {
+    switch (action.type) {
+        case BUILD_SEARCH: {
+            return action.payload;
+        }
+        case CLEAR_SEARCH: {
+            return initialState;
+        }
+        default: {
+            return state;
+        }
+    }
+
+}
