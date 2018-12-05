7bd9ff0895d92463ea6505c41d055e9e39e7fcc9
Jon Kollari <joni.kollari@canada.ca>
2018-12-05 19:41:44 +0000

    issue 18676: graph multiple elements + clear individual charts + predefined graphs

--- src/lib/core/obs-util/src/unit-code-conversion/index.ts
+++ src/lib/core/obs-util/src/unit-code-conversion/index.ts
@@ -1,10 +1,14 @@
-export { CodeSources ,
-         CodeTypes ,
-         CodeResult ,
-         FromUnits ,
-         ToUnits ,
-         UnitConversion ,
-         ConvertedValues } from './unit-code-conversion.model.ts';
+export {
+    CodeSources,
+    CodeTypes,
+    CodeResult,
+    FromUnits,
+    ToUnits,
+    UnitConversion,
+    ConvertedValues,
+} from './unit-code-conversion.model';
 export { UnitCodeConversionService } from './unit-code-conversion.service';
-export { UpdatePreferredUnitsAction,
-         preferredUnitsReducer } from './unit-code-conversion.reducer';
+export {
+    UpdatePreferredUnitsAction,
+    preferredUnitsReducer,
+} from './unit-code-conversion.reducer';
