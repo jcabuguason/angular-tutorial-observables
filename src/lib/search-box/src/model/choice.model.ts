a2a68c5f08960bb1c29efb793cb2b3c428abb8b8
May Lieu <may.lieu@canada.ca>
2019-02-25 15:56:28 +0000

    issue 19811: data access policy

--- src/lib/search-box/src/model/choice.model.ts
+++ src/lib/search-box/src/model/choice.model.ts
@@ -1,7 +1,16 @@
 export class ChoiceModel {
-  constructor(public label: string, public uri: string = '', public tooltip: string = '') {
+  constructor(
+    public label: string,
+    public uri: string = '',
+    public tooltip: string = '',
+    public icon: ChoiceIcon = new ChoiceIcon()
+  ) {
     if (this.uri === '') {
       this.uri = this.label;
     }
   }
+}
+
+export class ChoiceIcon {
+  constructor(public faIcon: string = '', public iconTooltip: string = '') {}
 }
