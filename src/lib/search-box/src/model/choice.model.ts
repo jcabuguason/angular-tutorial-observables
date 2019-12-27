90f023da96df5b61b25c65eb70004ab74c867208
May Lieu <may.lieu@canada.ca>
2019-12-27 19:47:41 +0000

    issue 22546: check multi selected values on searchbar

--- src/lib/search-box/src/model/choice.model.ts
+++ src/lib/search-box/src/model/choice.model.ts
@@ -4,13 +4,14 @@ export class ChoiceModel {
     public value: string = label,
     public uri: string = label,
     public tooltip: string = '',
-    public icon: ChoiceIcon = new ChoiceIcon()
-  ) {
-  }
+    public icon: ChoiceIcon = new ChoiceIcon(),
+  ) {}
 }
 
 export class ChoiceIcon {
-  constructor(public faIcon: string = '', public iconTooltip: string = '') { }
+  constructor(public faIcon: string = '', public iconTooltip: string = '') {}
 }
 
-export const LOADING_MODEL: ChoiceModel = new ChoiceModel('SEARCH_BAR.LOADING');
+export const NON_SELECTABLE = 'NON_SELECTABLE';
+
+export const LOADING_MODEL: ChoiceModel = new ChoiceModel('SEARCH_BAR.LOADING', NON_SELECTABLE);
